ebe951989a5c679793858c0534ebf2c5d56d1366
Ibrahima Sory Diallo <isorydiallo05@gmail.com>
2026-01-04 16:21:11 +0000

    Basic setup

---
+++ routes/main.py
@@ -0,0 +1,8 @@
+from flask import Blueprint, jsonify, render_template, request
+
+main_bp = Blueprint("main", __name__)
+
+
+@main_bp.route("/")
+def index():
+    return render_template("index.html")
