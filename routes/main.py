547595557fbd7db9b97405b09d0737ab5e9c0ecd
Ibrahima Sory Diallo <isorydiallo05@gmail.com>
2026-01-09 11:23:52 +0000

    Ajout de certains éléments dans les fichiers __init__.py pour faciliter l'import

--- routes/main.py
+++ routes/main.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 
-# will need to import jsonify, request and maybe joinedload later
+# will need to import jsonify, request and maybe joinedload late, depending
 
 main_bp = Blueprint("main", __name__)
 
@@ -8,3 +8,8 @@ main_bp = Blueprint("main", __name__)
 @main_bp.route("/")
 def index():
     return render_template("index.html")
+
+
+@main_bp.route("/profile")
+def profile():
+    return render_template("profile.html")
